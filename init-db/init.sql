18032804c6c528f94909e181c8ed15f3216d91d7
Lợi Trần <loivantran10012002@gmail.com>
2026-02-24 19:43:51 +0700

    feat: Add initial database setup script for development and production environments with corresponding users.

--- init-db/init.sql
+++ init-db/init.sql
@@ -1,16 +1,12 @@
 -- Create Databases
 CREATE DATABASE IF NOT EXISTS smart_class_dev;
 CREATE DATABASE IF NOT EXISTS smart_class_pro;
-CREATE DATABASE IF NOT EXISTS smart_class_test_1;
-CREATE DATABASE IF NOT EXISTS smart_class_test_2;
 
--- Create Dev User and Grant Permissions
+-- Create Dev User
 CREATE USER IF NOT EXISTS 'dev_user'@'%' IDENTIFIED BY 'dev_password_123';
 GRANT ALL PRIVILEGES ON smart_class_dev.* TO 'dev_user'@'%';
-GRANT ALL PRIVILEGES ON smart_class_test_1.* TO 'dev_user'@'%';
-GRANT ALL PRIVILEGES ON smart_class_test_2.* TO 'dev_user'@'%';
 
--- Create Pro User and Grant Permissions
+-- Create Pro User
 CREATE USER IF NOT EXISTS 'pro_user'@'%' IDENTIFIED BY 'pro_password_123';
 GRANT ALL PRIVILEGES ON smart_class_pro.* TO 'pro_user'@'%';
 
